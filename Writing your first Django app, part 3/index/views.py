98f8a5cd0912a649fd5bacd33f4076c587fbb695
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2024-11-24 10:58:54 +0600

    Topic Cover
    1 Create django venv 
    
    2 Install python, Django etc
    
    3 Create new app
    
    4 Create more view functions
    
    5 Use to HTTP Response direct massage output 
    
    6 Int type key pass & show this output massage or key

---
+++ Writing your first Django app, part 3/index/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+
+# create more views hear ! 
+# pass the int type urls id and call id 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
